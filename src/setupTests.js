7732ce4b09710bb60d63865a1816394a1bb543f3
jpham7436 <jpham7436@sdsu.edu>
2025-10-22 22:48:33 -0700

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,17 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom'
+
+// Mock lucide-react so every named icon becomes a lightweight <span />
+jest.mock('lucide-react', () => {
+  const React = require('react')
+  return new Proxy(
+    {},
+    {
+      get: (_target, iconName) =>
+        // Avoid JSX here to be extra safe in setup files
+        (props) => React.createElement('span', { 'data-icon': String(iconName), ...props }),
+    }
+  )
+})
+
+// Freeze time so "Today's Medications" logic is deterministic (a Wednesday)
+jest.useFakeTimers().setSystemTime(new Date('2025-10-22T12:00:00Z'))
